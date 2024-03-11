207177a4dc37379d2e48c1cd856ef28187eeb6d1
PrathamSalunkhe <prathamsalunkhe@dnkmail.in>
2024-03-11 17:55:34 +0530

    basic program

---
+++ basic/hello.c
@@ -0,0 +1,12 @@
+//this program will print the integer which user will enter
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    //ask user to enter the number
+    printf("Enter a number");
+    scanf("%d", &i);
+    printf("The number is : %d", i);
+    return 0;
+}
