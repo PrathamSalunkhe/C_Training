246fed303a268038eca234c9b3dab8f3b7085913
PrathamSalunkhe <prathamsalunkhe@dnkmail.in>
2024-03-11 18:19:29 +0530

    basic c questions

---
+++ basic_c_questions/firstp.c
@@ -0,0 +1,9 @@
+//what would be the o/p of this code
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%d", printf("%s","Hello World!"));  //%d prints integer and %s prints string
+    return 0;
+}
---
+++ basic_c_questions/thirdp.c
@@ -0,0 +1,11 @@
+//this program is written to operate 
+
+#include <stdio.h>
+
+int main()
+{
+    char c = 255;      //character can hold 256 value
+    c = c + 10;        //255+10=265    o/p will be 9
+    printf("%d", c);
+    return 0; 
+}
